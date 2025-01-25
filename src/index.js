2536b67e5bfbd4d4d43a2935a0faaa74630b5303
Kamiltczarnik <kamiltczarnik@gmail.com>
2025-01-25 17:36:10 -0600

    refresh bug fixing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 // index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 import Background from "./components/Background";
 import AnimatedRouter from "./AnimatedRouter";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router basename="/">
+  <Router>
     <Background>
       <AnimatedRouter />
     </Background>
