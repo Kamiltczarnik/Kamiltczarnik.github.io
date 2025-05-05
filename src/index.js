ae6f1ab0aa012ff2fb08431bcfaf2340e3cc441f
Kamiltczarnik <kamiltczarnik@gmail.com>
2025-05-04 20:03:57 -0500

    sitemap setup / removing hash router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import Background from "./components/Background";
 import AnimatedRouter from "./AnimatedRouter";
